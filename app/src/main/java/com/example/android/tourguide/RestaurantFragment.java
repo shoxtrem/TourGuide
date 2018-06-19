f6db81691628e356d18cc3be3cdec1328a12eebc
Shoxtrem <stanislas97@live.fr>
2018-06-19 14:48:21 +0200

    missing descriptions formatting

--- app/src/main/java/com/example/android/tourguide/RestaurantFragment.java
+++ app/src/main/java/com/example/android/tourguide/RestaurantFragment.java
@@ -17,9 +17,9 @@ public class RestaurantFragment extends Fragment {
 
         final ArrayList<ItemActivity> itemActivities = new ArrayList<>();
         itemActivities.add(new ItemActivity("La Panacée", "Modern art museum with a hip cafe", R.drawable.panacee));
-        itemActivities.add(new ItemActivity("Restaurant Leclere", "", R.drawable.restaurant_leclere));
-        itemActivities.add(new ItemActivity("Pastis Restaurant", "", R.drawable.restaurant_pastis));
-        itemActivities.add(new ItemActivity("Restaurant l'Angelus", "", R.drawable.restaurant_angelus));
+        itemActivities.add(new ItemActivity("Restaurant Leclere", "Local gastronomy", R.drawable.restaurant_leclere));
+        itemActivities.add(new ItemActivity("Pastis Restaurant", "Gastronomic restaurant", R.drawable.restaurant_pastis));
+        itemActivities.add(new ItemActivity("Restaurant l'Angelus", "traditional cuisine", R.drawable.restaurant_angelus));
         itemActivities.add(new ItemActivity("Léon de Bruxelles", "Mussels and french fries", R.drawable.leon_de_bruxelles));
         itemActivities.add(new ItemActivity("La Boucherie", "Hamburger and meat restaurant", R.drawable.restaurant_la_boucherie));
 
